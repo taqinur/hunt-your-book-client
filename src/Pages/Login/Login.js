1ae875dec475199db7bd7e97aa164e6b1939e14a
Taqinur <taqi1662@gmail.com>
2022-11-26 02:09:24 +0600

    added signup page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
 
-    const { register } = useForm();
+    const { formState: { errors }, register, handleSubmit } = useForm();
     const handleLogin = data => {
         console.log(data);
     }
@@ -13,23 +13,25 @@ const Login = () => {
         <div className='my-10 flex justify-center items-center'>
             <div className='w-3/6 lg:w-2/6 py-4 px-8 border-2 border-emerald-300'>
                 <h2 className="text-3xl text-center">Log In</h2>
-                <form onSubmit={handleLogin}>
+                <form onSubmit={handleSubmit(handleLogin)}>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input {...register("email")} type="email" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        <input {...register("email", {required: "Email Address is required"})} type="email" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Your Password</span>
                         </label>
-                        <input {...register("password")} type="password" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                        <input {...register("password",{ required: "Password is required"})} type="password" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
                         <label className="label">
                             <span className="label-text">Forgot Password?</span>
                         </label>
                     </div>
                     <input className='btn btn-primary w-full' type="submit" />
+                    {errors.email && <p className='text-red-600' role="alert">Alert: {errors.email?.message}</p>}
+                    {errors.password && <p className='text-red-600' role="alert">Alert: {errors.password?.message}</p>}
                 </form>
                 <p className="text-center my-4">Do not have an account? <Link className='text-emerald-600' to={'/signup'}>Sign Up Here</Link></p>
                 <button className="btn btn-outline btn-primary w-full">Login with Google</button>
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+
+    const { formState: { errors }, register, handleSubmit } = useForm();
+    const handleSignup = data => {
+        console.log(data);
+    }
+
     return (
-        <div>
-            uppppppppppppppppppp
+        <div className='my-10 flex justify-center items-center'>
+            <div className='w-3/6 lg:w-2/6 py-4 px-8 border-2 border-emerald-300'>
+                <h2 className="text-3xl text-center">Sign Up</h2>
+                <form onSubmit={handleSubmit(handleSignup)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input {...register("email", { required: "Email Address is required" })} type="email" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Your Password</span>
+                        </label>
+                        <input {...register("password", { required: "Password is required" })} type="password" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    </div>
+
+                    <div className="form-control">
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Buyer</span>
+                            <input {...register("role")} type="radio" value="buyer" className="radio checked:bg-blue-500" checked />
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <label className="label cursor-pointer">
+                            <span className="label-text">Seller</span>
+                            <input {...register("role")} type="radio" value="seller" className="radio checked:bg-blue-500" />
+                        </label>
+                    </div>
+                    <input className='btn btn-primary w-full' type="submit" />
+                    {errors.email && <p className='text-red-600' role="alert">Alert: {errors.email?.message}</p>}
+                    {errors.password && <p className='text-red-600' role="alert">Alert: {errors.password?.message}</p>}
+                </form>
+                <p className="text-center my-4">Have an account? <Link className='text-emerald-600' to={'/login'}>Log In Here</Link></p>
+            </div>
         </div>
     );
 };
